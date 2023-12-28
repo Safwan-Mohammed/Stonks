f7c0bd2ce8cb18bc18048a75460fc5ea31f1bcea
Safwan-Mohammed <safwanhanif19@gmail.com>
2023-12-29 00:05:51 +0530

    Animation Added

--- Components/helpers/HomeAnimation.js
+++ Components/helpers/HomeAnimation.js
@@ -1,32 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { Animated, View } from "react-native";
-import {LinearGradient} from "expo-linear-gradient";
+import { Animated, View, useWindowDimensions } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-const HomeAnimation = function (){
-//   const [animatedValue] = useState(new Animated.Value(0));
+const HomeAnimation = function () {
+  const [animatedValue] = useState(new Animated.Value(0));
+  const { height, width } = useWindowDimensions()
+  const val1 = Math.floor(height / 5)  
+  const val2 = Math.floor(height / 3)  
+  useEffect(() => {
+    const animateGradient = () => {
+      Animated.loop(
+        Animated.timing(animatedValue, {
+          toValue: 1,
+          duration: 10000,
+          useNativeDriver: false,
+        })
+      ).start();
+    };
 
-//   const startAnimation = () => {
-//     Animated.timing(animatedValue, {
-//       toValue: 1,
-//       duration: 3000,
-//       useNativeDriver: false,
-//     }).start(() => {
-//       // Optionally, you can add a callback function when the animation completes
-//     });
-//   };
+    animateGradient();
+  }, [animatedValue]);
 
-//   useEffect(() => {
-//     startAnimation();
-//   }, []); // Run the animation once when the component mounts
+  const translateY = animatedValue.interpolate({
+    inputRange: [0, 0.25, 0.5, 0.75, 1],
+    outputRange: [0, val1, val2, val1, 0],
+  });
 
   return (
-    <View style={{ flex: 1, position: 'absolute', height: 1000, width:500 }}>
-      <LinearGradient
-        // colors={["#4c669f", "#3b5998", "#192f6a"]}
-        colors={["#000000", "#00334E", "#1F4E5F" ]}
-        locations={[0.1, 0.5, 0.9]}
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      />
+    <View style={{ flex: 1, position: "absolute", height: height, width: width }}>
+      <Animated.View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          transform: [{ translateY }],
+        }}
+      >
+        <LinearGradient
+          colors={["#000000", "#00334E", "#1F4E5F"]}
+          locations={[0, 0.3, 0.6]}
+          style={{ flex: 1, width: "100%" }}
+        />
+      </Animated.View>
     </View>
   );
 };
